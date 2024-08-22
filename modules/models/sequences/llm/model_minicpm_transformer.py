a100afa3b2739bdcd4bfd99146d02ee34ff0e8ad
Justin Eric <justinlie777@goole.com>
2024-08-22 11:23:55 +0800

    add minicpm metric

--- modules/models/sequences/llm/model_minicpm_transformer.py
+++ modules/models/sequences/llm/model_minicpm_transformer.py
@@ -53,8 +53,19 @@ class MiniCPMTransformerModel(BaseModel):
         print('[Transformer] Human:', history[-1][0])
         print('[Transformer] Assistant: ', end='', flush=True)
         for new_text in res:
-            bot_message += new_text
-            print(new_text, flush=True, end='')
+            print(new_text, end='', flush=True)
+            if len(new_text) == 0:
+                continue
+            if new_text != ' ':
+                bot_message += new_text
+
+            try:
+                res.__next__()
+            except StopIteration:
+                end_time = time.time()
+                cost_time = round(end_time-start_time, 3)
+                words_count = len(bot_message)
+                single_word_cost_time = round((end_time-start_time)/len(bot_message), 3)
 
             yield bot_message, cost_time, words_count, single_word_cost_time
 
